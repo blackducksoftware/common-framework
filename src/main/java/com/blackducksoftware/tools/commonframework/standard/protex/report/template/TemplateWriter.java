fc4e5a709421e8d55f913a0e1c5e10485944d42b
garoushanian <garoushanian@blackducksoftware.com>
2015-11-30 10:28:37 -0500

    #10: Minor logging update;

--- src/main/java/com/blackducksoftware/tools/commonframework/standard/protex/report/template/TemplateWriter.java
+++ src/main/java/com/blackducksoftware/tools/commonframework/standard/protex/report/template/TemplateWriter.java
@@ -435,7 +435,7 @@ public class TemplateWriter<T extends TemplatePojo> {
 	    value = (ret == null) ? "" : ret.toString();
 
 	} catch (Exception e) {
-	    log.warn("Unable to reflectively get value for method: " +   lookupMappingName + " - POJO Class: " + pojo.getClass().getSimpleName());
+	    log.warn("Unable to reflectively get value for method: " +   lookupMappingName + " - POJO: " + pojo.getClass());
 	}
 
 	return value;
