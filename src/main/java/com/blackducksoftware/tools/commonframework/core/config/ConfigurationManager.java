75da304cc4e5f6028ba19adb0278f8b80bf2087c
garoushanian <garoushanian@blackducksoftware.com>
2015-11-30 09:02:53 -0500

    #10: Minor logging update;

--- src/main/java/com/blackducksoftware/tools/commonframework/core/config/ConfigurationManager.java
+++ src/main/java/com/blackducksoftware/tools/commonframework/core/config/ConfigurationManager.java
@@ -557,9 +557,9 @@ public abstract class ConfigurationManager extends ConfigConstants implements
 			if (mappings.containsKey(columnKey)) {
 			    String existingMethod = mappings.get(columnKey);
 			    if (!mappingMethod.equals(existingMethod)) {
-				throw new IllegalArgumentException(
+				throw new IllegalArgumentException("The key [" + 
 					columnKey
-						+ " is mapped more than once to non-unique methods.");
+						+ "] is mapped more than once to non-unique methods.");
 			    }
 			}
 
